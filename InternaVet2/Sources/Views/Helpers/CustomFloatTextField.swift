0ba881e06ffbd419ac9fdafdeb8b9bf342bcc192
Jorge Xavier <mr.jorge.xavier@gmail.com>
2017-08-21 00:27:49 -0300

    implementado layout da tela de cadastrar proprietario, #02;

--- InternaVet2/Sources/Views/Helpers/CustomFloatTextField.swift
+++ InternaVet2/Sources/Views/Helpers/CustomFloatTextField.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 import JVFloatLabeledTextField
+import InputMask
 
 class CustomFloatTextField: JVFloatLabeledTextField {
+    @IBInspectable var maskText: String? = nil {
+        didSet{
+            self.updateMask()
+        }
+    }
+    
+    var maskDelegate: MaskedTextFieldDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,7 +32,7 @@ class CustomFloatTextField: JVFloatLabeledTextField {
     func setupViews(){
         self.borderStyle = .none
         self.layer.cornerRadius = 5
-        self.layer.borderWidth = 2
+        self.layer.borderWidth = 1
         self.layer.shadowOffset = CGSize(width: 2, height: 4)
         self.layer.shadowColor = Colors.darkGreen.cgColor
         self.layer.borderColor = Colors.lightGreen.cgColor
@@ -32,17 +40,30 @@ class CustomFloatTextField: JVFloatLabeledTextField {
         self.placeholderColor = Colors.clearLightGreen
         self.floatingLabelTextColor = Colors.darkGreen
         self.floatingLabelActiveTextColor = Colors.lightGreen
+        self.floatingLabelYPadding = 2
         self.backgroundColor = Colors.mainLight
         
         self.clipsToBounds = true
     }
     
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: 5, dy: 0)
+        return self.textRectForBounds(b:bounds)
     }
     
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: 5, dy: 0)
+        return self.textRectForBounds(b:bounds)
     }
-
+    
+    fileprivate func textRectForBounds(b bounds: CGRect) -> CGRect {
+        let needsTextRepositioning = !(self.text?.isEmpty ?? false)
+        let topYDelta: CGFloat = needsTextRepositioning ? 4 : 0
+        return bounds.insetBy(dx: 5, dy: 0).insetBy(tY: topYDelta)
+    }
+    
+    fileprivate func updateMask() {
+        if let mask = maskText {
+            self.maskDelegate = MaskedTextFieldDelegate(format: mask)
+            self.delegate = self.maskDelegate
+        }
+    }
 }
